433f484917593c889f9fe492e144436e60573e2a
Andriy <devsokol@ukr.net>
2020-02-12 14:05:40 +0200

    add structure to file core 2

---
+++ app/Http/Controllers/CoreController.php
@@ -0,0 +1,20 @@
+<?php
+
+class App {
+    public function test() {
+        return object[
+            [
+                'id' => 1,
+                'name' => 'car1'
+            ],
+            [
+                'id' => 2,
+                'name' => 'car2'
+            ],
+            [
+                'id' => 3,
+                'name' => 'car3'
+            ]
+        ];
+    }
+}
